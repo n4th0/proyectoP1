720402c413e97ad0f096ba049420fc9396337471
Ariden <andresradoo@gmail.com>
2023-11-23 20:11:46 +0100

    actualizacion andrei

--- proyect/menu_proy.c
+++ proyect/menu_proy.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-void menu();
+int menu(char *m);
 
-void menu(){
+int menu(char *m){
 
     char x;
     bool s;
 
+    *m = 0;
     s = false;
 
     while (s == false){
@@ -18,40 +19,55 @@ void menu(){
         printf(".\n");
         printf(".\n");
 
-        printf("Bienvenido/a a *nombre_del_juego*. Dentro de *nombre_del_juego* podrás seleccionar varios modos de juego, cada uno de los cuales dispone de varios mapas a tu elección.\n");
-        printf("Introduce I para ver las instrucciones.\n");
-        printf("Introduce J para ver los modos de juego.\n");
-        printf("Introduce S para salir.\n");
+        printf("Bienvenido/a a...\n");
+        printf("\n      _____ ______   ________  ___      ___ _______\n     |\\   _ \\  _   \\|\\   __  \\|\\  \\    /  /|\\  ___ \\\n     \\ \\  \\\\\\__\\ \\  \\ \\  \\|\\  \\ \\  \\  /  / | \\   __/|\n      \\ \\  \\\\|__| \\  \\ \\  \\\\\\  \\ \\  \\/  / / \\ \\  \\_|/__\n       \\ \\  \\    \\ \\  \\ \\  \\\\\\  \\ \\    / /   \\ \\  \\_|\\ \\\n        \\ \\__\\    \\ \\__\\ \\_______\\ \\__/ /     \\ \\_______\\\n         \\|__|     \\|__|\\|_______|\\|__|/       \\|_______|\n");
+        printf("\n ____ \n||I ||\n||__|| Instrucciones. \n|/__\\|\n");
+        printf(" ____ \n||J ||\n||__|| Modos de juego. \n|/__\\|\n");
+        printf(" ____ \n||S ||\n||__|| Salir. \n|/__\\|\n");
+        printf("\n");
 
         scanf(" %c", &x);
 
         switch(x){
 
+	    case 'i':
             case 'I': 
                 printf("\e[1;1H\e[2J");
-                printf("mamabicho\n");
-                printf("Introduce cualquier tecla para salir.\n");
+                printf("En MOVE tendrás *N* modos de juego para elegir.\n--------------------------\n-------------------------\nEl primer modo de juego se llama 'LABIRINTH', el cual consiste en la resolución de un laberinto, pero tendrás que darte prisa, ya que perderás si no lo resuelves antes de que se acabe el tiempo.\n-------------------------\n-------------------------\nEl segundo modo de juego *...*.\n-------------------------\n-------------------------\nEl tercer modo de juego *...*.\n-------------------------\n-------------------------\nY por último, el cuarto modo de juego es *...*.\n-------------------------\n-------------------------\nPara moverte, tendrás que utiizar las teclas W,A,S y D, que van en las direcciones izquierda(A), arriba(W), derecha(D) y abajo(S).");
+                printf("\n\nIntroduce cualquier tecla para salir.\n");
                 scanf(" %c", &x);
                 if(x != '0') s = false;
             break;
             
+            case 'j':
             case 'J':
                 printf("\e[1;1H\e[2J");
-                printf("mamaguevo\n");
-                printf("Introduce cualquier tecla para salir.\n");
+                printf(" ____ \n||1 ||\n||__|| Resuelve el laberinto. \n|/__\\|\n");
+                printf(" ____ \n||2 ||\n||__|| Escapa del enemigo. \n|/__\\|\n");
+                printf(" ____ \n||3 ||\n||__|| ************ \n|/__\\|\n");
+                printf(" ____ \n||4 ||\n||__|| ************ \n|/__\\|\n");
+                printf(" ____ \n||S ||\n||__|| Volver al menú. \n|/__\\|\n\n");
                 scanf(" %c", &x);
-                if (x != '0') s = false;
+            	if (x == 's') s = false;
+            	else if (x == '1' || x == '2' || x == '3' || x == '4'){
+            		*m = x;
+            		s = true;
+            	}
             break;
 
+	    case 's':
             case 'S':
                 s = true;
-            break;
+                printf("\e[1;1H\e[2J");
+            return 0;
 
         }
     }
 }
 
 int main(){
-    menu();
+    char m;
+
+    menu(&m);
     return 0;
 }

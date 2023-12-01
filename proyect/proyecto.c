5a55210d55300d0700958bc98ba6c97f5ff1853c
n4th0 <nathanmoyses@gmail.com>
2023-12-01 16:57:13 +0100

    matriz a la mitad, editor mejorado

--- proyect/proyecto.c
+++ proyect/proyecto.c
@@ -12,12 +12,12 @@
 /////
 
 #define tamanox 27
-#define tamanoy 100
+#define tamanoy 50
 
-#define POSICION_INICIAL_PLMANX 24
-#define POSICION_INICIAL_PLMANY 97
+#define POSICION_INICIAL_PLMANX 25
+#define POSICION_INICIAL_PLMANY 48
 #define POSICION_INICIAL_SAVEZONEX 1
-#define POSICION_INICIAL_SAVEZONEY 3
+#define POSICION_INICIAL_SAVEZONEY 1
 
 void dibujar_tablero(int tablero[tamanox][tamanoy]) {
   printf("\n");
@@ -28,40 +28,40 @@ void dibujar_tablero(int tablero[tamanox][tamanoy]) {
       switch (tablero[i][j]) {
       case 0:
         // nada
-        printf(" ");
+        printf("  ");
         break;
       case 1:
         // pared
         printf("\x1b[37m"
                "\x1b[47m"
-               " "
+               "  "
                "\x1b[0m");
         break;
       case 2:
         // enemigo
         printf("\x1b[31m"
                "\x1b[41m"
-               " "
+               "  "
                "\x1b[0m");
         break;
       case 3:
         // plman
         printf("\x1b[33m"
                "\x1b[43m"
-               " "
+               "  "
                "\x1b[0m");
         break;
       case 4:
         // save zone
         printf("\x1b[32m"
                "\x1b[42m"
-               " "
+               "  "
                "\x1b[0m");
         break;
       default:
         // codigo de error
         printf("\x1b[31m"
-               "E"
+               "EE"
                "\x1b[0m");
 
         break;
@@ -97,24 +97,19 @@ char preguntardireccion() {
   return direccion;
 }
 
-
-
 /// patorjk.com/software/taag para usar el menu del mapa
 // usar small keyboard para poner las opciones
 // usar o8 para poner el "menu mapa"
 // larry 3d para el nombre del juego
 
 int main() {
-char c = ' ';
+  char c = ' ';
   // posición inicial de plman
   int posx = POSICION_INICIAL_PLMANX, posy = POSICION_INICIAL_PLMANY;
-  int posy1 = posy - 1;
 
   int posex = 8, posey = 5;
-  int posey1 = posey - 1;
 
   int posSx = POSICION_INICIAL_SAVEZONEX, posSy = POSICION_INICIAL_SAVEZONEY;
-  int posSy1 = posSy - 1;
 
   int tablero2[tamanox][tamanoy];
 
@@ -122,7 +117,6 @@ char c = ' ';
   // segun el estado final (si ha ganado ) enviar un mensaje
   // y si ha perdido otro
   int game = 1;
-
 
   int estado = 1;
   char opcion;
@@ -132,25 +126,21 @@ char c = ' ';
 
   do {
 
-    printf("\e[1;1H\e[2J");
-
     switch (opcion) {
     case '1':
       mapa1(tablero2);
       break;
-
     }
 
+    printf("\e[1;1H\e[2J");
+
     // indica la posicion de plman
-    tablero2[posx][posy1] = 3;
     tablero2[posx][posy] = 3;
 
     // indica la posicion de enemigos
-    tablero2[posex][posey1] = 2;
     tablero2[posex][posey] = 2;
 
     // indica la posición de la save zone
-    tablero2[posSx][posSy1] = 4;
     tablero2[posSx][posSy] = 4;
 
     dibujar_tablero(tablero2);
@@ -160,33 +150,24 @@ char c = ' ';
     // pase la librería) actualización de plman
     switch (c) {
     case 'w':
-      if (tablero2[posx - 1][posy] != 1 && tablero2[posx - 1][posy1] != 1) {
+      if (tablero2[posx-1][posy] != 1) {
         posx = posx - 1;
       }
       break;
     case 's':
 
-      if (tablero2[posx + 1][posy] != 1 && tablero2[posx + 1][posy1] != 1) {
+      if (tablero2[posx + 1][posy] != 1) {
         posx = posx + 1;
       }
       break;
     case 'd':
-      if (tablero2[posx][posy + 2] != 1 && tablero2[posx][posy + 1] != 1) {
-        posy = posy + 2;
-        posy1 = posy1 + 2;
-      } else if (tablero2[posx][posy + 1] != 1) {
+      if (tablero2[posx][posy + 1] != 1) {
         posy = posy + 1;
-        posy1 = posy1 + 1;
       }
       break;
     case 'a':
-      if (tablero2[posx][posy1 - 2] != 1 && tablero2[posx][posy1 - 1] != 1) {
-        posy = posy - 2;
-        posy1 = posy1 - 2;
-      } else if (tablero2[posx][posy1 - 1] != 1) {
-
+      if (tablero2[posx][posy - 1] != 1) {
         posy = posy - 1;
-        posy1 = posy1 - 1;
       }
       break;
     }
@@ -205,13 +186,11 @@ char c = ' ';
     }
 
     // colisiones
-    if (posex == posx && posey1 == posy1 || posex == posx && posey == posy1 ||
-        posex == posx && posey1 == posy) {
+    if ( posex == posx && posey == posy ) {
       game = 2;
     }
     // colision save zone
-    if (posSx == posx && posSy1 == posy1 || posSx == posx && posSy == posy1 ||
-        posSx == posx && posSy1 == posy) {
+    if ( posSx == posx && posSy == posy ) {
       game = 3;
     }
   } while (game == 1);
